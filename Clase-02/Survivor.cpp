493f62fe1c0075c209cefa7beba1441b52d52cf9
Juan Diego Haro <harodiego96@gmail.com>
2026-08-19 07:23:22 +0000

    Clase 02

---
+++ Clase-02/Survivor.cpp
@@ -0,0 +1,13 @@
+// USFQ - Colegio de Ciencias e Ingeniería
+// CMP-2702: Programación Avanzada en C++
+// Laboratorio 2
+// Estudiante: [Tu Nombre]
+// Profesor: Juan Diego Haro (jharo@asig.com.ec)
+
+include <iostream>
+include <string>
+
+int main(){
+    // Su código aqui
+    return 0;
+}
